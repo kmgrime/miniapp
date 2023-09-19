cd4c0581fe72cd2959a1b846a6b86c2ffe2132f3
u58306 <u58306@N54959X.energycorp.com>
2023-09-19 20:55:24 +0200

    several changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+
+
 # SQLite3 config
 
-import sqlite3
-
-conn = sqlite3.connect('mydatabase.db')
+conn = sqlite3.connect("mydatabase.db")
 cursor = conn.cursor()
 
 # Check if the 'contacts' table already exists
@@ -10,14 +12,16 @@ cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='cont
 table_exists = cursor.fetchone()
 
 if not table_exists:
-    cursor.execute('''
+    cursor.execute(
+        """
         CREATE TABLE contacts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             address TEXT,
             phone_number TEXT
         )
-    ''')
+    """
+    )
 
 conn.commit()
 conn.close()
@@ -25,32 +29,33 @@ conn.close()
 
 # Flask config
 
-import sqlite3
-from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        address = request.form['address']
-        phone_number = request.form['phone_number']
 
-        conn = sqlite3.connect('mydatabase.db')
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        address = request.form["address"]
+        phone_number = request.form["phone_number"]
+
+        conn = sqlite3.connect("mydatabase.db")
         cursor = conn.cursor()
 
-        cursor.execute('''
+        cursor.execute(
+            """
             INSERT INTO contacts (name, address, phone_number)
             VALUES (?, ?, ?)
-        ''', (name, address, phone_number))
+        """,
+            (name, address, phone_number),
+        )
 
         conn.commit()
         conn.close()
 
-        return redirect(url_for('success'))
+        return redirect(url_for("success"))
 
-    return '''
+    return """
     <h2>Contact Form</h2>
     <form method="post">
         <label for="name">Name:</label>
@@ -67,25 +72,28 @@ def index():
     <form action="/view" method="get">
         <input type="submit" value="View Form">
     </form>
-    '''
+    """
 
-@app.route('/success', methods=['GET', 'POST'])
+
+@app.route("/success", methods=["GET", "POST"])
 def success():
-    if request.method == 'POST':
-        return redirect(url_for('index'))  # Redirect back to the form
+    if request.method == "POST":
+        return redirect(url_for("index"))  # Redirect back to the form
     return 'Form submitted successfully!<br><form method="post"><input type="submit" value="Return to Form"></form>'
 
-@app.route('/view', methods=['GET'])
+
+@app.route("/view", methods=["GET"])
 def view():
-    conn = sqlite3.connect('mydatabase.db')
+    conn = sqlite3.connect("mydatabase.db")
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM contacts')
+    cursor.execute("SELECT * FROM contacts")
     data = cursor.fetchall()
 
     conn.close()
 
-    return render_template('view.html', data=data)
+    return render_template("view.html", data=data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
